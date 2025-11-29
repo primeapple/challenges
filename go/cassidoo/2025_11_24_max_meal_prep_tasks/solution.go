2bae06779f8afe7c4eddf1533414fcb33880245f
primeapple <toni.mueller.web@mailbox.org>
2025-11-29 23:19:28 +0100

    feat: Add cassidoo 2025_11_24_max_meal_prep_tasks

---
+++ go/cassidoo/2025_11_24_max_meal_prep_tasks/solution.go
@@ -0,0 +1,13 @@
+package maxMealPrepTasks
+
+// Idea: Gready Algorithm
+// Sort by endTime ascending
+// Do the first task, delete all tasks that are not possible anymore
+// Do it until queue is empty
+func MaxMealPrepTasks(tasks []Task) TasksOrder {
+	chosenTasks := []string{}
+
+
+
+	return TasksOrder{count: len(chosenTasks), chosen: chosenTasks}
+}
---
+++ go/cassidoo/2025_11_24_max_meal_prep_tasks/types.go
@@ -0,0 +1,12 @@
+package maxMealPrepTasks
+
+type Task struct {
+	name string
+	start int
+	end int
+}
+
+type TasksOrder struct {
+	count int
+	chosen []string
+}
